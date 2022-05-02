675ec78eb1e75c65e2344a96cc22b8f75ad111d0
Gergoa123 <gergo3124@gmail.com>
2022-05-02 18:16:11 +0300

    Add project files.

---
+++ Exercitiul 3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercitiul_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti numarul!");
+            int numar = Convert.ToInt32(Console.ReadLine());
+            int ultimaCifra = numar % 10;
+            Console.WriteLine("Ultima cifra a numarului introdus = " + ultimaCifra);
+        }
+    }
+}
